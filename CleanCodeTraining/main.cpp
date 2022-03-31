8c66c248266e964a9c1ab4e9c900f4cda82d63d8
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-03-31 17:07:58 +0300

    cleaned main

--- CleanCodeTraining/main.cpp
+++ CleanCodeTraining/main.cpp
@@ -2,13 +2,8 @@
 #include "ApprovalTests.v.10.12.2.hpp"
 
 int main(int argc, char* argv[]) {
-	// your setup ...
-	auto directoryDisposer =
-		ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
-
 	int result = Catch::Session().run(argc, argv);
 
-	// your clean-up...
 
 	return result;
 }
